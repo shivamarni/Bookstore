f252c4209ce17131e2bcbf20477b62aed7b30053
shivamarni <shivamarni10@gmail.com>
2020-05-13 01:00:39 +0530

    [shiva] add . LoginDto class

---
+++ src/main/java/com/bridgelabz/bookstore/dto/LoginDto.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.bookstore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LoginDto {	
+	private String email;
+	private String password;
+}

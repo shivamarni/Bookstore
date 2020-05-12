e6ad11783ade57d9284e274c8146804d63489674
tkoteswararao131997 <tkoteswararao131997@gmail.com>
2020-05-12 23:37:11 +0530

    [koti]  swagger implemented

--- src/main/java/com/bridgelabz/bookstore/BookstoreApplication.java
+++ src/main/java/com/bridgelabz/bookstore/BookstoreApplication.java
@@ -2,8 +2,10 @@ package com.bridgelabz.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BookstoreApplication {
 
 	public static void main(String[] args) {

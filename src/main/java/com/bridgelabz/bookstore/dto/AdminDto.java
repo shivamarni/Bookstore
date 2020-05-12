0dc7375e01d2678f8b083dd4940be5c2b4d1ca93
shivamarni <shivamarni10@gmail.com>
2020-05-13 01:10:54 +0530

    [shiva] refractor. changes done

--- src/main/java/com/bridgelabz/bookstore/dto/AdminDto.java
+++ src/main/java/com/bridgelabz/bookstore/dto/AdminDto.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

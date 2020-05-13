64eb3b690862ded6f45b7f996894cff9a40af34a
shivamarni <shivamarni10@gmail.com>
2020-05-13 19:21:38 +0530

    [shiva] refractor. changes done

--- src/main/java/com/bridgelabz/bookstore/repository/SellerRepository.java
+++ src/main/java/com/bridgelabz/bookstore/repository/SellerRepository.java
@@ -1,5 +1,40 @@
 package com.bridgelabz.bookstore.repository;
 
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import com.bridgelabz.bookstore.entity.Seller;
+
+
+@Repository
 public class SellerRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public Seller save(Seller seller) {
+
+		Session session = entityManager.unwrap(Session.class);
+
+		session.saveOrUpdate(seller);
+
+		return seller;
+
+	}
+	
+	public Optional<Seller> getSellerById(Long sellerId) {
+		Session session = entityManager.unwrap(Session.class);
+		System.out.println(sellerId);
+		return session.createQuery("FROM Seller where sellerId =:id").setParameter("id", sellerId).uniqueResultOptional();
+	}
+
+	
+	
+	
 
 }

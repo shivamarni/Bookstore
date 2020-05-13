1e5d0d22f3a8202dcfd9486a18d4466eecb6c9a4
shivamarni <shivamarni10@gmail.com>
2020-05-13 19:22:59 +0530

    [shiva] refractor. changes done

--- src/main/java/com/bridgelabz/bookstore/service/SellerService.java
+++ src/main/java/com/bridgelabz/bookstore/service/SellerService.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.bookstore.service;
 
-public class SellerService {
+import java.util.List;
 
+import com.bridgelabz.bookstore.dto.LoginDto;
+import com.bridgelabz.bookstore.dto.UserDto;
+import com.bridgelabz.bookstore.entity.Seller;
+import com.bridgelabz.bookstore.exception.BookStoreException;
+
+public interface SellerService {
+    Seller registerSeller(UserDto userdto)throws BookStoreException;
+	Seller loginSeller(LoginDto dto) throws BookStoreException;
+	Seller verify(String token) throws BookStoreException;
+	Seller getSellerById(Long sellerId) throws BookStoreException;
+	void deleteSeller(Long userId);
+	public List<Seller> getSellers();
+
+	
 }

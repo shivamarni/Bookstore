efa7662a84cec311d549b111e457165a06e7106d
tkoteswararao131997 <tkoteswararao131997@gmail.com>
2020-05-14 20:08:51 +0530

    [mukesh]bookDto validations done

--- src/main/java/com/bridgelabz/bookstore/dto/BookDto.java
+++ src/main/java/com/bridgelabz/bookstore/dto/BookDto.java
@@ -2,6 +2,8 @@ package com.bridgelabz.bookstore.dto;
 
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookDto {
+	@NotNull(message = "book name required for add book")
 	private String bookName;
-
+	
+	@NotNull(message = "author name required for add book")
 	private String bookAuthor;
-
+	
+	@NotNull(message = "book price required for add book")
 	private Double bookPrice;
 
+	@NotNull(message = "book quantity required for add book")
 	private Long noOfBooks;
 
 	private String bookDescription;

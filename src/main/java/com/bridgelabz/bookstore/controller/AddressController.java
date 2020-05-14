3a7abd8a84a06915d5d6b9a850c5d54f35f88915
tkoteswararao131997 <tkoteswararao131997@gmail.com>
2020-05-14 11:21:44 +0530

    [koti]address controller done

--- src/main/java/com/bridgelabz/bookstore/controller/AddressController.java
+++ src/main/java/com/bridgelabz/bookstore/controller/AddressController.java
@@ -1,9 +1,18 @@
 package com.bridgelabz.bookstore.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +29,48 @@ public class AddressController {
 	@Autowired
 	private AddressServiceImpl addressimpl;
 	@PostMapping("address/addAddress")
-	public ResponseEntity<Response> addAddress(@RequestBody AddressDto addressDto,@RequestHeader String token) throws BookStoreException
+	public ResponseEntity<Response> addAddress(@Valid @RequestBody AddressDto addressDto,@RequestHeader String token,BindingResult result) throws BookStoreException
 	{
+		if (result.hasErrors())
+		return new ResponseEntity<Response>(new Response(result.getAllErrors().get(0).getDefaultMessage(),null,400),HttpStatus.BAD_REQUEST);
 		Address address=addressimpl.addAddress(addressDto, token);
 		return new ResponseEntity<Response>(new Response("address added", address, 200),HttpStatus.CREATED);
 	}
+	
+	@PutMapping("address/updateAddress")
+	public ResponseEntity<Response> updateAddress(@Valid @RequestBody AddressDto addressDto,@RequestHeader String token,@RequestHeader Long addressId,BindingResult result) throws BookStoreException
+	{
+		if (result.hasErrors())
+		return new ResponseEntity<Response>(new Response(result.getAllErrors().get(0).getDefaultMessage(),null,400),HttpStatus.BAD_REQUEST);
+		Address address=addressimpl.updateAddress(addressDto, token,addressId);
+		return new ResponseEntity<Response>(new Response("address updated", address, 200),HttpStatus.CREATED);
+	}
+	@GetMapping("address/getAddressById")
+	public ResponseEntity<Response> getAddressById(Long addressId,Long userId) throws BookStoreException
+	{
+		Address address=addressimpl.getAddressById(addressId, userId);
+		return new ResponseEntity<Response>(new Response("address is",address, 200),HttpStatus.CREATED);
+	}
+	
+	@GetMapping("address/getAllAddresses")
+	public ResponseEntity<Response> getAllAddresses(String token) throws BookStoreException
+	{
+		List<Address> addresses=addressimpl.getAllAddresses(token);
+		return new ResponseEntity<Response>(new Response("address is",addresses, 200),HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("address/deleteAddressById/{addressId}")
+	public ResponseEntity<Response> deleteAddress(String token,@PathVariable("addressId") Long addressId) throws BookStoreException
+	{
+		addressimpl.deleteAddress(token,addressId);
+		return new ResponseEntity<Response>(new Response("address deleted",null, 200),HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("address/deleteAllAddresses")
+	public ResponseEntity<Response> deleteAllAddresses(String token) throws BookStoreException
+	{
+		addressimpl.deleteAllAddresses(token);
+		return new ResponseEntity<Response>(new Response("all addresses deleted",null, 200),HttpStatus.CREATED);
+	}
+	
 }
--- src/main/java/com/bridgelabz/bookstore/repository/AddressRepository.java
+++ src/main/java/com/bridgelabz/bookstore/repository/AddressRepository.java
@@ -1,11 +1,25 @@
 package com.bridgelabz.bookstore.repository;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bridgelabz.bookstore.entity.Address;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address,String> {
+	
+	@Query(value="select * from address where address_id=?1 and user_id=?2",nativeQuery=true)
+	Optional<Address> getAddressById(Long addressId, Long userId);
+	
+	@Transactional
+	@Modifying
+	@Query(value="delete from address where user_id=?1",nativeQuery=true)
+	void deleteAllAddresses(Long userId);
 
 }
